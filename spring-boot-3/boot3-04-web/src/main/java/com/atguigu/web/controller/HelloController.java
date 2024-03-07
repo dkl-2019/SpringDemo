86d8e39406397ac70e5c00a9ae2e34a1db20b889
dkl <1834359580@qq.com>
2024-03-07 17:19:30 +0800

    路径匹配，内容商定 2024.03.07

--- spring-boot-3/boot3-04-web/src/main/java/com/atguigu/web/controller/HelloController.java
+++ spring-boot-3/boot3-04-web/src/main/java/com/atguigu/web/controller/HelloController.java
@@ -11,6 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    /**
+     * 默认使用新版 PathPatterParser 进行路径匹配
+     * 性能高
+     * 不能匹配 ** 在中间的情况，剩下的和 antPathMatcher 语法兼容
+     *
+     * 使用默认的路径匹配规则，是由 PathPatternParser 提供的
+     * 如果中间需要有 ** ，替换成 antPathMatcher 模式即可
+     * @param request
+     * @param path
+     * @return
+     */
+
     @GetMapping("/a*/b?/{p1:[a-f]+}/**")
     public String hello(HttpServletRequest request, @PathVariable("p1") String path) {
         log.info("路径变量p1: {}", path);
